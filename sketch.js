5396d90a87bf6e078a441d95aff2fecdbf64dd09
sophiekearse <sophiekearse@gmail.com>
2026-05-19 20:16:21 +0000

    created the ghosts and established the variables for the game + started the startGame function

--- sketch.js
+++ sketch.js
@@ -1,25 +1,55 @@
-await Canvas();
-world.gravity.y = 10;
+let gameState = "startScreen"; 
+let ghost;
 
-let ball = new Sprite();
-ball.diameter = 50;
-ball.img = '🤪';
+let vines; 
+let gravestones;
 
-let groundA = new Sprite();
-groundA.x = -120;
-groundA.width = 220;
-groundA.rotation = 30;
-groundA.physics = STATIC;
+let score = 0; 
+let gameSpeed = 1; 
+let obstacleTimer = 0; 
 
-let groundB = new Sprite();
-groundB.x = 120;
-groundB.width = 220;
-groundB.rotation = -30;
-groundB.physics = STATIC;
+//Will add images from Piskel
+let ghostImg; 
+let gravestoneImg; 
 
-q5.update = function () {
-	background('skyblue');
-	text('click to jump!', 0, -50);
+function images() {
+    ghostImg = loadImage(""); 
+    gravestoneImg = loadImage(""); 
+    backgroundImg = loadImage("");
+ }
 
-	if (mouse.presses()) ball.vel.y = -5;
-};
+function 
+function startGame() { 
+    gameState = "play"; 
+    score = 0; 
+    ghost.x = 150; 
+    ghost.y = 250; 
+    ghost.vel.y = 0; 
+    gravestones.removeAll(); 
+    vines.removeAll(); }
+
+// await Canvas();
+// world.gravity.y = 10;
+
+// let ball = new Sprite();
+// ball.diameter = 50;
+// ball.img = '🤪';
+
+// let groundA = new Sprite();
+// groundA.x = -120;
+// groundA.width = 220;
+// groundA.rotation = 30;
+// groundA.physics = STATIC;
+
+// let groundB = new Sprite();
+// groundB.x = 120;
+// groundB.width = 220;
+// groundB.rotation = -30;
+// groundB.physics = STATIC;
+
+// q5.update = function () {
+// 	background('skyblue');
+// 	text('click to jump!', 0, -50);
+
+// 	if (mouse.presses()) ball.vel.y = -5;
+// };
